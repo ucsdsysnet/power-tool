35546178f321123393cdef0d0a6c4d16702e5530
Malcolm McSwain <mrmcswai@c09-10.sysnet.ucsd.edu>
2020-05-25 13:12:13 -0700

    Bug fixes

--- api/server.js
+++ api/server.js
@@ -1,11 +1,75 @@
+//this is connecting our front-end to the Node JS server (using Express)
+// the React server and the Node server are talking to each other (call it API)
+	// the front-end sends a request to the API to respond with a .png graph
+
+// we collect the 
 const express = require('express')
-const cors = require('cors')
+const { execFile } = require('child_process')
 
 const app = express()
-app.use(cors())
-
 const port = process.env.PORT || 9000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('//power', (req, res) => {
+
+	// req represents the incoming requests (from the form)
+	// res represents the outgoing requests (to the form)
+
+	// store request params in a variable	
+	// to capture the request parameters collected from the form, we can use req.query
+	// req.query stores the parameters provided by the form
+
+	// GET /?servers[]=b09-xx&startTime=mm-dd-yy::MM:ss:SS&endTime=mm-dd-yy::MM:ss:SS
+	// req.query.startTime, req.query.endTime, req.query.servers are holding the inputdata
+
+
+	// spawn child process passing in request params and store it in a variable
+	
+	const child = execFile('/home/cleung/ERSP/scripts/visualization/extract.sh', [
+		req.query.startTime, 
+		req.query.endTime, 
+		req.query.serverOne, 
+		req.query.serverTwo,
+		req.query.serverThree,
+		req.query.serverFour
+	], (error, stdout, stderr) => {
+		if (error) {
+			res.send('error.png')
+			throw error;
+		}
+		res.send(stdout)
+	})
+})
+
+app.get('//cpu', (req, res) => {
+
+	// req represents the incoming requests (from the form)
+	// res represents the outgoing requests (to the form)
+
+	// store request params in a variable	
+	// to capture the request parameters collected from the form, we can use req.query
+	// req.query stores the parameters provided by the form
+
+	// GET /?servers[]=b09-xx&startTime=mm-dd-yy::MM:ss:SS&endTime=mm-dd-yy::MM:ss:SS
+	// req.query.startTime, req.query.endTime, req.query.servers are holding the inputdata
+
+
+	// spawn child process passing in request params and store it in a variable
+	
+	const child = execFile('/home/cleung/ERSP/scripts/visualization/cpu_extract.sh', [
+		req.query.startTime, 
+		req.query.endTime, 
+		req.query.serverOne, 
+		req.query.serverTwo,
+		req.query.serverThree,
+		req.query.serverFour
+	], (error, stdout, stderr) => {
+		if (error) {
+			res.send('error.png')
+			throw error;
+		}
+		res.send(stdout)
+	})
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,99 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, FormGroup, Label, Input } from 'reactstrap';
+import React, { useState } from 'react';
+import { Container, Row, Col, FormGroup, Label, Input, Button, Navbar, NavbarBrand } from 'reactstrap';
 import Datetime from 'react-datetime';
+import moment from 'moment';
+import Logo from './logo.png';
 import './App.css';
+import NavbarText from 'reactstrap/lib/NavbarText';
 
 function App() {
 
-  const [response, setResponse] = useState('');
+  const [serverOne, setServerOne] = useState('');
+  const [serverTwo, setServerTwo] = useState('');
+  const [serverThree, setServerThree] = useState('');
+  const [serverFour, setServerFour] = useState('');
+  const [startTime, setStartTime] = useState('');
+  const [endTime, setEndTime] = useState('');
+  const [powerUri, setPowerUri] = useState('');
+  const [cpuUri, setCpuUri] = useState('');
 
-  useEffect(() => {
-    fetch("http://localhost:9000/")
+  /*useEffect(() => {
+    fetch("http://power-tool.sysnet.ucsd.edu/api")
       .then(res => res.text())
       .then(res => setResponse(res));
-  }, []);
+  }, []);*/
+
+  const generateGraphs = e => {
+    e.preventDefault();
+
+    let servers = [];
+    if (serverOne === 'on') servers[0] = 1; else servers[0] = 0;
+    if (serverTwo === 'on') servers[1] = 1; else servers[1] = 0;
+    if (serverThree === 'on') servers[2] = 1; else servers[2] = 0;
+    if (serverFour === 'on') servers[3] = 1; else servers[3] = 0;
+
+    console.log(`http://power-tool.sysnet.ucsd.edu/api/power?startTime=${startTime}&endTime=${endTime}&serverOne=${servers[0]}&serverTwo=${servers[1]}&serverThree=${servers[2]}&serverFour=${servers[3]}`);
+    console.log(`http://power-tool.sysnet.ucsd.edu/api/cpu?startTime=${startTime}&endTime=${endTime}&serverOne=${servers[0]}&serverTwo=${servers[1]}&serverThree=${servers[2]}&serverFour=${servers[3]}`);
+
+    fetch(`http://power-tool.sysnet.ucsd.edu/api/power?startTime=${startTime}&endTime=${endTime}&serverOne=${servers[0]}&serverTwo=${servers[1]}&serverThree=${servers[2]}&serverFour=${servers[3]}`)
+      .then(res => res.text())
+      .then(res => setPowerUri(res));
+
+    fetch(`http://power-tool.sysnet.ucsd.edu/api/cpu?startTime=${startTime}&endTime=${endTime}&serverOne=${servers[0]}&serverTwo=${servers[1]}&serverThree=${servers[2]}&serverFour=${servers[3]}`)
+      .then(res => res.text())
+      .then(res => setCpuUri(res));
+  }
 
   return (
-    <Container>
-      <Row style={{marginTop: '50px'}}>
-        <Col sm="4">
-          <h3>Servers to analyze</h3>
-          <FormGroup check>
-            <Label check>
-              <Input type="checkbox" />{' '}
-              b09-11
-            </Label>
-            <br/>
-            <Label check>
-              <Input type="checkbox" />{' '}
-              b09-12
-            </Label>
-            <br/>
-            <Label check>
-              <Input type="checkbox" />{' '}
-              b09-13
-            </Label>
-            <br/>
-            <Label check>
-              <Input type="checkbox" />{' '}
-              b09-14
-            </Label>
-          </FormGroup>
-        </Col>
-        <Col sm="4">
-          <h3>Start date/time</h3>
-          <Datetime dateFormat="MM/DD/YYYY" timeFormat="hh:mm:ss"/>
-        </Col>
-        <Col sm="4">
-          <h3>End date/time</h3>
-          <Datetime />
-        </Col>
-      </Row>
-      <Row style={{marginTop: '50px'}}>
-        <h3>Result will be graphed here.</h3>
-        <p>{response}</p>
-      </Row>
-    </Container>
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/"><img src={Logo} style={{height: '50px', marginLeft: '250px'}}/></NavbarBrand>
+        <NavbarText>Power Metrics Tool</NavbarText>
+      </Navbar>
+      <Container>
+        <Row style={{marginTop: '50px'}}>
+          <Col sm="4">
+            <h3>Servers to analyze</h3>
+            <FormGroup check>
+              <Label check>
+                <Input type="checkbox" onChange={e => setServerOne(e.target.value)}/>{' '}
+                c09-13
+              </Label>
+              <br/>
+              <Label check>
+                <Input type="checkbox" onChange={e => setServerTwo(e.target.value)}/>{' '}
+                c09-14
+              </Label>
+              <br/>
+              <Label check>
+                <Input type="checkbox" onChange={e => setServerThree(e.target.value)}/>{' '}
+                c09-15
+              </Label>
+              <br/>
+              <Label check>
+                <Input type="checkbox" onChange={e => setServerFour(e.target.value)}/>{' '}
+                c09-16
+              </Label>
+            </FormGroup>
+          </Col>
+          <Col sm="4">
+            <h3>Start date/time</h3>
+            <Datetime dateFormat="MM/DD/YYYY" timeFormat="HH:mm:ss" onChange={e => setStartTime(moment(e).format('MM/DD/YYYY::HH:mm:ss'))}/>
+          </Col>
+          <Col sm="4">
+            <h3>End date/time</h3>
+            <Datetime dateFormat="MM/DD/YYYY" timeFormat="HH:mm:ss" onChange={e => setEndTime(moment(e).format('MM/DD/YYYY::HH:mm:ss'))}/>
+          </Col>
+        </Row>
+        <Row style={{marginTop: '50px'}}>
+            <Button color="primary" onClick={generateGraphs}>Graph</Button>
+        </Row>
+        <Row style={{marginTop: '50px'}}>
+            <img src={powerUri && `http://power-tool.sysnet.ucsd.edu/power-graphs/${powerUri}`} />
+            <img src={cpuUri && `http://power-tool.sysnet.ucsd.edu/cpu-graphs/${cpuUri}`} />
+        </Row>
+      </Container>
+    </div>
   );
 }
 
